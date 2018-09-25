33927866ae76b7769399d6a34da94ae7f312a16a
dennismuturia <dmuturia@mo-de.local>
2018-09-25 17:32:11 +0300

    Used a cond situation

--- src/my_app/core.clj
+++ src/my_app/core.clj
@@ -5,20 +5,3 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Its you"))
-;;Since there are no loops we will devise a new way to loop
-(defn fizz-buzz [j]
-    (loop [i j]
-    (if (<= j 100)
-        (do
-           (if (and (= (rem i 3) 0)(= (rem i 5) 0))
-                (print "FizzBuzz")
-                (if (= (rem i 3) 0)
-                     (print "Fizz")
-                     (if (= (rem i 5) 0)
-                        (print "Buzz")
-                     ))
-                 )
-           )
-    )
-        (recur (inc i)))
-    )
--- src/my_app/fizzbuzz.clj
+++ src/my_app/fizzbuzz.clj
@@ -1,4 +1,4 @@
-;;This is a simple fizzbuzz applicatiion in clojure. Previously written in Nightcode
+;;This is a simple fizzbuzz applicatiion in clojure. Previously written in Nightcode. The wrong way
 (defn fizz-buzz [j]
     (loop [i j]
         (do
@@ -12,5 +12,23 @@
                  )
            )
         (recur (inc i)))
+    )
+)
+;; We will use macros to make things easier
+(defn fizz-buzz-cond [k]
+    ;;Make the loop start at one
+    (loop [i 1]
+        (if (<= i k)
+            ;; Initialize a do
+            (do
+            ;; Cond is like a if statement condotion to replace a dozen if statements
+                (cond
+                    (and (= (rem i 3) 0) (= (rem i 5) 0)) (println "FizzBuzz")
+                    (= (rem i 3) 0)(println "Fizz")
+                    (= (rem i 5) 0)(println "Buzz")
+                )
+                (recur (inc i))
+            )
+        )
     )
 )
